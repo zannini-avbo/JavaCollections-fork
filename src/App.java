b5dc4e814aeb982ffaca9d740136ab6c67771233
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2025-05-09 11:08:58 +0000

    starting code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // test Hello World
         System.out.println("Hello, World!");
+
+        // test MyIntArray
+        MyIntArray a = new MyIntArray();
+        for (int i=0; i<10; i++) a.add(i);
+        System.out.println(a);
+
     }
 }
