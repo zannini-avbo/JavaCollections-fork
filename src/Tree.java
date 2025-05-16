a75b8255c7e7e36517839dd2c24d970db701441b
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2025-05-16 11:09:00 +0000

    visita ordinata

--- src/Tree.java
+++ src/Tree.java
@@ -7,7 +7,7 @@ public class Tree<T extends Comparable<T>> {
     }
 
     private void recursiveAdd( Treenode<T> p, T value ) {
-        if (p.getValue().compareTo(value)<0) {
+        if (p.getValue().compareTo(value)>0) {
             if (p.getLeft()==null) {
                 p.setLeft(new Treenode<>(value));
             } else recursiveAdd( p.getLeft(), value );
@@ -25,8 +25,19 @@ public class Tree<T extends Comparable<T>> {
 
     @Override
     public String toString() {
-        return "Tree [root=" + root + "]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tree [");
+        visitaSottoAlbero(root, sb);
+        if (sb.length() > 6) sb.setLength(sb.length() - 2); // Rimuove l'ultima virgola e spazio
+        sb.append("]");
+        return sb.toString();
     }
-    
-    
+
+    private void visitaSottoAlbero(Treenode<T> node, StringBuilder sb) {
+        if (node == null) return;
+        visitaSottoAlbero(node.getLeft(), sb);
+        sb.append(node.getValue()).append(", ");
+        visitaSottoAlbero(node.getRight(), sb);
+    }
+
 }
